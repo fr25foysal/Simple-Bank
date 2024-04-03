c70e68e37c1a4b1696edf3283cfff0ca957e18a1
Foysal Rahman <fr25foysal@gmail.com>
2024-04-03 15:08:14 +0600

    Entered login page validation

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const loginButton = document.getElementById('btn-login');
 const newP = document.createElement('p');
-    newP.innerText = 'Invalid Username or password';
+    
     newP.className = 'text-white text-sm'
 loginButton.addEventListener('click', function(){
     const loginUsername = document.getElementById('login-username');
@@ -10,8 +10,12 @@ loginButton.addEventListener('click', function(){
     
     if (loginUsername.value === 'khulJa' && loginPassword.value === 'simSim') {
         window.location.href = 'dashboard.html';
+    }else if(loginUsername.value === '' || loginPassword.value === ''){
+        newP.innerText = "Please enter Username and Password";
+        loginBox.appendChild(newP)
     }
     else{
+        newP.innerText = 'Invalid Username or password';
         loginBox.appendChild(newP)
     }
     loginUsername.value = '';
